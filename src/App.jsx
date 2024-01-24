1717795719208ac27cefe657173a65ebde9ecf10
Marta <tran.marta@gmail.com>
2024-01-24 19:03:28 +0100

    config

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API = '/assets/api.json';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+function App()
+{
+  const [songs, setSongs] = useState([]);
+  const [selectedSong, setSelectedSong] = useState(0);
+  const audioRef = useRef();
+
+  useEffect(
+    () => {
+      fetch(API).then(response => response.json()).then(data => setSongs(data))
+      []);
+    }, []);
+
+  if (songs.length < 1)
+  {
+    return (
+      <div>Ładowanie danych...</div>
+    )  
+  }
+
+	return (
+    <div className='min-h-screen bg-gray-900 text-white justify-center items-center flex-col m-auto'>
+      <secton className='bg-black p-8 text-center'>
+        <h1 className='text-3xl font-bold mb-4'>Spotify</h1>
+        <img src={ songs[selectedSong].cover } className='mx-auto my-4 w-64 object-cover shadow-lg' />
+        <button onClick={ () => audioRef.current.play() }>PLAY</button>
+        <button onClick={ () => audioRef.current.pause() }>Pause</button>
+        <audio ref={ audioRef }>
+          <source src={ songs.[selectedSong].audio } />
+        </audio>
+      </secton>
+	  Spotify
+      <section>
+        <h1 className='text-2xl font-bold mb-4 text-center'>
+          Lista piosenek:
+        </h1>
+        <ul className='flex flex-col space-y-2'>
+          { songs.map((song, index) => (
+            <li className={ 'text-center py-2 px-4 cursor-pointer rounded-lg' } key={ song.id }>
+              { song.title } by { song.author }
+            </li>
+          )) }
+        </ul>
+      </section>
+    </div>
   )
 }
-
-export default App
